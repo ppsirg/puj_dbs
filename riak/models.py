b6ff56d84bb84ac32b20f41f5b37ae09d69932bc
Pedro Rivera <ppsirg@gmail.com>
2020-09-15 23:09:32 -0500

    populate debugged, views debugged

--- riak/models.py
+++ riak/models.py
@@ -14,9 +14,9 @@ fake = Faker(['es_ES', 'es_MX', 'en_US', 'en_GB', 'pt_BR'])
 
 
 # define network data
-num_users = 500
+num_users = 200
 num_cities = 50
-msg_per_user = 100
+msg_per_user = 30
 max_replies_per_user = 5
 max_followers = 10
 format_timestamp = '%Y/%m/%d_%H:%M:%S:%f'
@@ -29,20 +29,20 @@ class RiakBase(object):
         self.client = RiakClient(protocol='http', host='127.0.0.1', http_port=8098)
 
 
-class Cities(RiakBase):
+class Cities():
     """Manage cities to calculate cities ranking of tweets.
     """
-    BUCKET = 'cities'
+    BUCKET = 'citiesY'
 
-    def __init__(self):
-        super().__init__(self)
+    def __init__(self, cl):
+        self.client = cl
         self.bucket = self.client.bucket(self.BUCKET)
         # check if data
-        keys = self.bucket.get_keys()
-        if self.BUCKET in keys:
+        keys = self.bucket.get(self.BUCKET)
+        if keys:
             self.value = self.bucket.get(self.BUCKET)
         else:
-            cities_list = {f'{fake.language()} {fake.city()}': 0 for a in range(num_cities)}
+            cities_list = {f'{fake.city()}': 0 for a in range(num_cities)}
             cities = self.bucket.new(self.BUCKET, cities_list)
             cities.store()
             self.value = cities
@@ -63,15 +63,15 @@ class Cities(RiakBase):
         return rank[:num]
 
 
-class User(RiakBase):
+class User():
     INFO_BUCKET = 'users'
     MSG_BUCKET = 'tweet'
     LAST_BUCKET = 'tweet_last10'
 
-    def __init__(self, email, city=None):
-        super().__init__(self)
+    def __init__(self, cl, email, city=None):
+        self.client = cl
         self.repr = f'user_{email}'
-        self.cities = Cities()
+        self.cities = Cities(self.client)
         # initialize bucket
         self.info_bucket = self.client.bucket(self.INFO_BUCKET)
         self.msg_bucket = self.client.bucket(self.MSG_BUCKET)
@@ -104,26 +104,34 @@ class User(RiakBase):
     def add_message(self, message, reply_to=None, timestamp=None):
         # build message body
         timestamp = datetime.now().strftime(format_timestamp) if not timestamp else timestamp
-        owner_info = {k,v for k,v in self.info.data.items() if k != 'following'}
+        owner_info = {k:v for k,v in self.info.data.items() if k != 'following'}
         message_body = {
             'timestamp': timestamp,
             'content': message,
-            'messageType': choices(MESSAGE_TYPES)[0],
+            'messageType': (MESSAGE_TYPES[1] if message else MESSAGE_TYPES[2]) if reply_to else MESSAGE_TYPES[0],
             'owner': owner_info
         }
         if reply_to:
             message_body['messageBase'] = reply_to
         # save message
-        new_msg = self.msg_bucket.new(msg_id, message_body)
-        new_msg.store()
+        messages_list = self.msg_bucket.get(self.repr)
+        if not messages_list.data:
+            messages_list = self.msg_bucket.new(
+                self.repr,
+                {'messages': [message_body]}
+                )
+            
+        else:
+            messages_list.data['messages'].insert(0, message_body)
+        messages_list.store()
         # update cities counter
         self.cities.increment(self.info.data['city'])
         # update last messages bucket
-        self.last_messages.data['data'].insert(message_body)
+        self.last_messages.data['data'].insert(0, message_body)
         if len(self.last_messages.data['data']) == 11:
             self.last_messages.data['data'].pop()
         self.last_messages.store()
-        return new_msg
+        return message_body
 
     def get_last_messages(self, num):
         last_messages = self.last_messages.data
@@ -133,23 +141,29 @@ class User(RiakBase):
             return last_messages.get('data')[:num]
 
     def follow_user(self, user_to_follow_id):
-        self.info['following'].append(user_to_follow_id)
+        self.info.data['following'].append(user_to_follow_id)
         self.info.store()
 
 
 def populate():
-    cities = Cities()
+    cl = RiakClient(protocol='http', host='127.0.0.1', http_port=8098)
+    cities = Cities(cl)
     users_list = []
     # create users
+    print('doing users')
     for i in range(num_users):
-        usr = User(i, fake.email())
+        usr = User(cl, fake.email())
         users_list.append(usr)
+        print(usr.info.data)
     # make users follow each others
+    print('doing follows')
     for usr in users_list:
         to_follow = choices(users_list, k=10)
         for tg in to_follow:
             usr.follow_user(tg.repr)
+        print(usr.info.data)
     # create messages
+    print('doing messages')
     for usr in users_list:
         num_msg = randint(5, msg_per_user)
         current = datetime.now()
@@ -161,22 +175,35 @@ def populate():
                 )
             # figure out if make a share or a replica out of this
             random_pick = randint(0,5)
+            print('original', new_msg)
             if random_pick == 2:
                 # its a replica
                 replicant = choices(users_list)[0]
                 reply_timestamp = current + timedelta(minutes=randint(3, 90))
+                original_msg_ref = '_'.join([new_msg['owner']['email'], new_msg['timestamp']])
                 new_reply = replicant.add_message(
                     fake.text(), 
                     timestamp=reply_timestamp.strftime(format_timestamp),
-                    reply_to=new_msg.data['id']
+                    reply_to={
+                        'ref': original_msg_ref, 
+                        'content': new_msg['content']
+                        }
                 )
+                print('reply', new_reply)
             elif random_pick == 3:
                 # its a share
                 replicant = choices(users_list)[0]
                 reply_timestamp = current + timedelta(minutes=randint(3, 90))
+                original_msg_ref = '_'.join([new_msg['owner']['email'], new_msg['timestamp']])
                 new_reply = replicant.add_message(
                     '', 
                     timestamp=reply_timestamp.strftime(format_timestamp),
-                    reply_to=new_msg.data['id']
+                    reply_to={
+                        'ref': original_msg_ref, 
+                        'content': new_msg['content']
+                        }
                 )
+                print('share', new_reply)
     
+if __name__ == "__main__":
+    populate()
--- riak/views.py
+++ riak/views.py
@@ -2,7 +2,7 @@ from models import User, Cities
 from pprint import pprint
 
 
-def get_own_msg(user_id):
+def get_own_msg(user_email):
     """  1.a
     Los últimos 10 mensajes que el usuario ha publicado, 
     en orden cronológico, empezando por el más  reciente.
@@ -10,13 +10,24 @@ def get_own_msg(user_id):
     usuario que lo  publicó. Si  son mensajes re-enviados, 
     indica el usuarioque publicó el mensaje original.
     """
-    usr = User(user_id)
+    usr = User(user_email)
     last_messages = usr.get_last_messages(10)
-    pprint(last_messages)
+    for msg in last_messages:
+        if msg['messageType'] == 'ORIGINAL':
+            print(msg['timestamp'])
+            print(msg['content'])
+        elif msg['messageType'] == 'REPLY':
+            print(msg['timestamp'])
+            print(msg['content'])
+            print('reply to ', msg['messageBase']['ref'].split('_')[0])
+            print(msg['messageBase']['content'])
+        elif msg['messageType'] == 'SHARE':
+            print('shared on ', msg['timestamp'])
+            print('from ', msg['messageBase']['ref'].split('_')[0])
+            print(msg['messageBase']['content'])
 
 
-
-def get_follower_msg(user_id):
+def get_follower_msg(user_email):
     """ 1.b
     Los últimos 5 mensajes que han publicado cada uno de
     los usuarios a los que sigue, agrupados por el usuario 
@@ -24,12 +35,16 @@ def get_follower_msg(user_id):
     alfabético y, los mensajes de cada usuario,en orden 
     cronológico,empezando por el más reciente
     """
-    usr = User(user_id)
-    for followed_id in usr.info['following_to']:
-        influencer = User(followed_id)
-        print(influencer.info['email'])
-        pprint(influencer.get_last_messages(5))
-
+    usr = User(user_email)
+    influencers = usr.info.data['following']
+    influencers.sort()
+    for followed_email in influencers:
+        influencer = User(followed_email)
+        print(influencer.info.data['email'])
+        last_messages = influencer.get_last_messages(5)
+        for msg in last_messages:
+            print(msg['messageType'], ' at ', msg['timestamp'])
+            print(msg['content'])
 
 
 def city_ranking():
